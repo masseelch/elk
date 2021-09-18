c6b5e9bc4c39eabed02a87e69d80f27707ab7cb8
Andrew Winterman <andrew.winterman@altruist.com>
2021-09-18 12:56:18 -0700

    add generate client to simple example

--- internal/simple/ent/entc.go
+++ internal/simple/ent/entc.go
@@ -6,8 +6,13 @@ package main
 import (
 	"entgo.io/ent/entc"
 	"entgo.io/ent/entc/gen"
+	"github.com/deepmap/oapi-codegen/pkg/codegen"
+	"github.com/deepmap/oapi-codegen/pkg/util"
 	"github.com/masseelch/elk"
+	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -21,5 +26,43 @@ func main() {
 	err = entc.Generate("./schema", &gen.Config{}, entc.Extensions(ex))
 	if err != nil {
 		log.Fatalf("running ent codegen: %v", err)
+	}
+
+	generateClient(err)
+}
+
+func generateClient(err error) {
+	swagger, err := util.LoadSwagger("./openapi.json")
+	if err != nil {
+		log.Fatalf("Failed to load swagger %v", err)
+	}
+
+	generated, err := codegen.Generate(swagger, "stub", codegen.Options{
+		GenerateChiServer:  false,
+		GenerateEchoServer: false,
+		GenerateClient:     true,
+		GenerateTypes:      true,
+		EmbedSpec:          false,
+		SkipFmt:            false,
+		SkipPrune:          false,
+		AliasTypes:         true,
+	})
+	if err != nil {
+		log.Fatalf("Generating client failed%v", err);
+	}
+
+	dir := filepath.Join(".", "stub")
+	stub := filepath.Join(".", "stub", "http.go")
+	perm := os.FileMode(0777)
+	err = os.MkdirAll(dir, perm)
+
+	if err != nil {
+		log.Fatalf("error creating dir: %s", err)
+	}
+
+	err = ioutil.WriteFile(stub, []byte(generated), perm)
+	if err != nil {
+		panic(err)
+		log.Fatalf("error writing generated code to file: %s", err)
 	}
 }
